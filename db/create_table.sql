31ae233f73c60f0e52d23e007f2eb9fea7ba6e54
Jaesung Ryu <cwryu6252@naver.com>
2023-05-18 17:16:15 +0900

    [add] create table sql

---
+++ db/create_table.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    description TEXT,
+    twitter_link VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE events (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    created_user_id INT,
+    title VARCHAR(255) NOT NULL,
+    thumbnail VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (created_user_id) REFERENCES users(id)
+);
+
+CREATE TABLE favorites (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    event_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (event_id) REFERENCES events(id)
+);
