49b1b2c7ed2e1485c28f7ab4a63b318c794ee29c
Jaesung Ryu <cwryu6252@naver.com>
2023-05-20 00:43:52 +0900

    update schema

--- backend/src/db/schema.py
+++ backend/src/db/schema.py
@@ -1,29 +1,24 @@
-import os
-
 from sqlalchemy import (
-    create_engine,
+    # create_engine,
     Column,
     Integer,
     String,
     Text,
     Boolean,
-    ForeignKey,
     TIMESTAMP,
+    ForeignKey,
 )
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 # SQLAlchemy 엔진 생성
-ID = os.getenv("DB_ID")
-PW = os.getenv("DB_PW")
-HOST = os.getenv("DB_HOST")
-engine = create_engine(f"mysql://{ID}:{PW}@{HOST}/tap_db?charset=utf8")
-
-# 모델을 위한 기본 베이스 선언
+# engine = create_engine("mysql://username:password@localhost/db_name")
+# Session = sessionmaker(bind=engine)
+# session = Session()
 Base = declarative_base()
 
 
-# 사용자 모델 정의
+# 스키마에 해당하는 모델 클래스 정의
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -31,7 +26,6 @@ class User(Base):
     created_at = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
 
 
-# 이벤트 모델 정의
 class Event(Base):
     __tablename__ = "events"
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -46,7 +40,6 @@ class Event(Base):
     created_user = relationship("User", backref="events")
 
 
-# 즐겨찾기 모델 정의
 class Favorite(Base):
     __tablename__ = "favorites"
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -57,7 +50,6 @@ class Favorite(Base):
     event = relationship("Event", backref="favorites")
 
 
-# 사용자 참여 모델 정의
 class UserParticipation(Base):
     __tablename__ = "user_participations"
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -68,5 +60,11 @@ class UserParticipation(Base):
     event = relationship("Event", backref="participations")
 
 
-# 테이블 생성
-Base.metadata.create_all(engine)
+class PreRegistration(Base):
+    __tablename__ = "pre_registrations"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    event_id = Column(Integer, ForeignKey("events.id"))
+    created_at = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
+    user = relationship("User", backref="pre_registrations")
+    event = relationship("Event", backref="pre_registrations")
